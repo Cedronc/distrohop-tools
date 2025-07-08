c36296e29b1a2b4b8951d98d4cc561fdbaff0fbf
Cedric <cedricmrtl@pm.me>
2025-07-09 00:38:16 +0200

    added vesktop

--- install-packages.sh
+++ install-packages.sh
@@ -3,26 +3,25 @@
 sudo pacman -Sy npm
 sudo pacman -S nvim
 sudo pacman -S brave
-sudo pacman -S discord
+flatpak install flathub dev.vencord.Vesktop
 sudo pacman -S ghostty
 sudo pacman -S lazygit
 
-sudo pacman -S ttf-font-awesome
-sudo pacman -S ttf-jetbrains-mono-nerd
-sudo pacman -S waybar
-sudo pacman -S wofi
+sudo pacman -S ttf-jetbrains-mono-nerd ttf-font-awesome
+sudo pacman -S waybar wofi
+sudo pacman -S swaync cliphist
+sudo pacman -S hyprlock hyprpaper hyprsunset hyprpicker hyprshot
+sudo pacman -S xdg-desktop-portal-hyprland
+sudo pacman -S pipewire wireplumber
 
-sudo pacman -S hyprshot
-sudo pacman -S swaync
-sudo pacman -S hyprlock
-sudo pacman -S hyprpaper
+
 sudo pacman -S starship
 
 
 yay -S neofetch
 yay -S spotify
 yay -S spicetify-cli
-# sudo chmod -R a+rwx /home/cedric/.config/spicetify/.
+
 spicetify config current_theme Gruvify
 spicetify apply
 
