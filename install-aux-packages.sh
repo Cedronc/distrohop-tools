d31e28b4e7589c4a1032276adbe6baea7a7e94b9
Cedric <cedricmrtl@pm.me>
2025-08-03 17:46:12 +0200

    fixed gruvify

--- install-aux-packages.sh
+++ install-aux-packages.sh
@@ -3,9 +3,25 @@
 
 yay -S neofetch
 yay -S spotify
+
+sudo chmod a+wr /opt/spotify
+sudo chmod a+wr /opt/spotify/Apps -R
+
 yay -S spicetify-cli
 yay -S overskride
 flatpak install flathub dev.vencord.Vesktop
+
+
+spicetify backup apply enable-devtools
+spicetify update
+
+curl -fsSL https://raw.githubusercontent.com/nimsandu/spicetify-bloom/main/install/install.sh | bash
+
+cd ~/.config/spicetify/Themes/
+git clone https://github.com/Skaytacium/Gruvify
+cd Gruvify
+npm i -g sass
+sass user.sass user.css
 
 spicetify config current_theme Gruvify
 spicetify apply
