38247a6c399563497aa84c329446e92891a6cbc9
Cedric <cedricmrtl@pm.me>
2025-08-04 13:07:29 +0200

    ccc

--- install-packages.sh
+++ install-packages.sh
@@ -7,11 +7,12 @@ sudo pacman -S brave
 sudo pacman -Sy proton-vpn-gtk-app network-manager-applet wireguard-tools systemd-resolvconf
 sudo pacman -S ghostty nautilus 
 sudo pacman -S lazygit
+sudo pacman -S fastfetch
 
 sudo pacman -S ttf-jetbrains-mono-nerd ttf-font-awesome
 sudo pacman -S waybar wofi cava
 sudo pacman -S swaync cliphist
-sudo pacman -S hyprlock hyprpaper hyprsunset hyprpicker hyprshot
+sudo pacman -S hyprlock hyprpaper hyprsunset hyprpicker hyprshot hypridle
 sudo pacman -S xdg-desktop-portal-hyprland
 sudo pacman -S pacman-contrib
 sudo pacman -S pipewire wireplumber
